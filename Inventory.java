c62c3435e9ed7ffe057d806a13cfd1626a5e9156
aveW618 <wangave27@7hills.org>
2025-10-01 18:42:23 -0400

    starting budget and inventory setup

--- Inventory.java
+++ Inventory.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Inventory {
-	public static void main(String [] args) {
+	//defining variables for inventory goods
+	int lemons;
+	int sugarCubes;
+	int iceCubes;
+	int cups;
 	
+	//main method begins
+	public static void main(String [] args) {
+		//create budget object
+		Budget userBudget = new Budget();
+		//set starting budget
+		userBudget = 20;
+		//start game
+		System.out.println("Welcome to the Lemonade Stand Game!");
+		System.out.println("Your starting budget is $" + userBudget);
+		
+		//Set inventory item prices
+		int priceLemons = 0.7;
+		int priceSugarCubes = 0.3;
+		int priceIceCubes = 0.06;
+		int priceCups = 0.08;
+			
+			
+		}
+	}
+}
