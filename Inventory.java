741e558932b8f33d8a9ea3237fe6ab42d69a6c11
aveW618 <wangave27@7hills.org>
2025-10-01 18:58:12 -0400

    added comments and outputted budget and inventory information for user

--- Inventory.java
+++ Inventory.java
@@ -25,50 +25,82 @@ public class Inventory {
 		
 		//ask user to buy lemons
 		System.out.print("How many lemons do you want to buy ($0.70 each)? ")
+		//Scanner class to get user input
 		int lemonsWanted = scanner.nextInt();
+		//calculate cost
 		int cost = lemonsWanted * priceLemons;
+		//compare cost to budget
 		if (cost <= userBudget) {
+			//add lemons to inventory
 			lemons += lemonsWanted;
+			//subtract cost from budget
 			userBudget -= cost;
 		} 
+		//if cost is over the budget, tell user
 		else {
 			System.out.println("Not enough money!");
 		}
 		
 		//ask user to buy sugar
 		System.out.print("How many sugar cubes do you want to buy ($0.30 each)? ")
+		//Scanner class to get user input
 		int sugarWanted = scanner.nextInt();
+		//calculate cost
 		int cost = sugarWanted * priceSugar;
+		//compare cost to budget
 		if (cost <= userBudget) {
+			//add sugar to inventory
 			sugar += sugarWanted;
+			//subtract cost from budget
 			userBudget -= cost;
 		} 
+		//if cost is over the budget, tell user
 		else {
 			System.out.println("Not enough money!");
 		}
 		
 		//ask user to buy ice
 		System.out.print("How many ice cubes do you want to buy ($0.06 each)? ")
+		//Scanner class to get user input
 		int iceWanted = scanner.nextInt();
+		//calculate cost
 		int cost = iceWanted * priceIce;
+		//compare cost to budget
 		if (cost <= userBudget) {
+			//add ice to inventory
 			ice += iceWanted;
+			//subtract cost from budget
 			userBudget -= cost;
 		} 
+		//if cost is over the budget, tell user
 		else {
 			System.out.println("Not enough money!");
 		}
 		
 		//ask user to buy cups
 		System.out.print("How many cups do you want to buy ($0.08 each)? ")
+		//Scanner class to get user input
 		int cupsWanted = scanner.nextInt();
+		//calculate cost
 		int cost = cupsWanted * priceCups;
+		//compare cost to budget
 		if (cost <= userBudget) {
+			//add cups to inventory
 			cups += cupsWanted;
+			//subtract cost from budget
 			userBudget -= cost;
 		} 
+		//if cost is over the budget, tell user
 		else {
 			System.out.println("Not enough money!");
 		}
+		
+		//print out final inventory and budget information for user
+		System.out.println("Final Inventory: ");
+		System.out.println("Lemons: " + lemonsWanted);
+		System.out.println("Ice: " + iceWanted);
+		System.out.println("Sugar: " + sugarWanted);
+		System.out.println("Cups: " + cupsWanted);
+		System.out.println("Money Left: $" + userBudget);
 	}
 }
