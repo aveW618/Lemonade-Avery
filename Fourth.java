32b06619e2afd7e31bf74efa7c373e7dacde0404
aveW618 <wangave27@7hills.org>
2025-10-01 10:10:34 -0400

    detele practice files

--- Fourth.java
+++
@@ -1,5 +0,0 @@
-public class Fourth.java {
-	public static void main (String [] args) {
-		System.out.println("Hello!"_;
-	}
-}
--- Third.java
+++
@@ -1,5 +0,0 @@
-public class Third.java {
-	public static void main (String [] args) {
-		System.out.println ("Hello!");
-	}
-}
